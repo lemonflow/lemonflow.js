f25382b143887955c9ee88c28d8898b41ffeb827
Philipp Fischer <philipp.fischer@daimler.com>
2013-12-23 21:40:55 +0100

    added "seff" reference - needs to be fixed - somehow still wrong scope

--- lemonflow/ui/base/OperatorStates.ts
+++ lemonflow/ui/base/OperatorStates.ts
@@ -1,190 +1,140 @@
 class OperatorStates extends OperatorInteraction {
-    active:boolean = false;
-    eventTypes = [];
-    _zbeData = {index:0,length:1};
-    _lastEvent = null;
     
-    _model = null;
-    _context = null;
-    _view;
+    active:boolean  = false;
+    eventTypes      = [];
+    zbeData         = {index:0,length:1};
+    lastEvent       = null;
     
-    changeWatchers;
+    context         = { "view": this._view, "state": "main" };
+    flow            = null;
+    _view           = null;
+    
+    changeWatchers  = [];
+
+    private self    = null;
     
     constructor() {
         super();
     }
     
-    setup(slots = null):void {
-        if(!this.hasOwnProperty("model")) return;
+    setup():void {
+        this.self = this;
         
-        //TODO more sophisticated check the structural correctness of the JSON model for UI flow
-        //for now we assume, it will have the numberended structure and capture type/reference errors by exception handling
-        //			var valid:boolean = true;
-        //			valid &&= this.hasOwnProperty("model");
-        //			for(var i:number=0; i< this._model.length; i++) { 	
-        //				valid &&= this["model"][i].hasOwnProperty("changes"); 
-        //				for(var k:number=0; k< this._model[i].changes.length; k++) 
-        //					valid &&= this._model[i].changes[k].hasOwnProperty("type")
-        //					for(var t:number=0; t<stateObjectTypes.length; t++) 
-        //						types.push(stateObjectTypes[t]);
-        //				}
-        //			}
-        //			if(!valid) return;
+        if (!this.flow) return;
         
-        
-        this._model = this["model"];
-        this._context =  this.hasOwnProperty("context")?this["context"]:(this.hasOwnProperty("transitioner")?this["transitioner"]:{"view":null, "state":"main"});
+        if (this.context.view != this._view)
+            this.context.view = this._view;
         
         //build events listener types
-        var types:Array = new Array();
-        for(var i:number=0; i< this._model.length; i++) {
-            for(var k:number=0; k< this._model[i].changes.length; k++) {
-                var stateObjectTypes = (this._model[i].changes[k].type instanceof Array ?  this._model[i].changes[k].type : [this._model[i].changes[k].type]);
-                for(var t:number=0; t<stateObjectTypes.length; t++) types.push(stateObjectTypes[t]);
+        var types = new Array();
+        for (var i = 0; i < this.flow.length; i++) {
+            for (var k = 0; k < this.flow[i].changes.length; k++) {
+                var stateObjectTypes = 
+                    (this.flow[i].changes[k].type instanceof Array ? this.flow[i].changes[k].type : [this.flow[i].changes[k].type]);
+                for (var t = 0; t < stateObjectTypes.length; t++)
+                    types.push(stateObjectTypes[t]);
             }
         }
-        types.sort(); //nice to have
-        
-        this.eventTypes = types.filter(function(e, i:number, a:Array):boolean { return a.indexOf(e) == i; });
-}
-
-processUserInput(e = null):void {
-    this._lastEvent = e;
-    this.update();
-}
-
-update(slots = null, currentIdx:Number = 0, nextIndex:Number = 0):void {
-    if(!this._model) this._model = this["model"];
-    if(!this._context) {
-        this._context =  this.hasOwnProperty("context")?
-            this["context"]:(this.hasOwnProperty("transitioner")?
-                             this["transitioner"]:{"view":null, "state":"main"});
+        types.sort();
+        this.eventTypes = types.filter(function (e, i, a) {
+            return a.indexOf(e) == i;
+        });
     }
-    if(this._context["view"]!=this._view) this._context["view"]= this._view; 
     
-    for(var i:number=0; i< this._model.length; i++) {
-        var s:Object = this._model[i];
-        if(this._context["state"] == s["state"] || s["state"] == "global") {
-            var q:Array = s["changes"]
-            for(var j:number=0; j< q.length; j++) {
-                var stateChange:Object = q[j];
+    processUserInput(e):void {
+        this.self.lastEvent = e;
+        this.self.update.call(this.self);
+    }
+    
+    update():void {
+        for (var i = 0; i < this.flow.length; i++) {
+            var s = this.flow[i];
+            
+            if (this.context["state"] == s["state"] || s["state"] == "global") {
                 
-                //iterate through all events and then trigger the actions and transitions
-                var types:Array = stateChange["type"] instanceof Array? stateChange["type"] : [stateChange["type"]];
-                for(var u:number=0; u<types.length; u++) {
-                    //current event?
-                    if(types[u] != this._lastEvent.type) continue;
-                    
-                    //guard is true?
-                    var guardValue:boolean = true;
-                    if(stateChange.hasOwnProperty("guard")){ 
-                        var f:Function = stateChange["guard"];
-                        var transtioner = this._context;
-                        guardValue = f();
-                    }
-                    if(!guardValue) continue;
-                    
-                    //do the state change
-                    //							trace("component: "+Object(this).toString()+"\t |  state: "+s["state"] + " | \t trigger: " +this._lastEvent.type );
-                    if(stateChange.hasOwnProperty("transition") && stateChange["transition"] != null) {
-                        for(var k:number=0; k< stateChange["transition"].length; k++) {
-                            (new stateChange["transition"][k](this._context)).run(this._lastEvent);
+                var q = s["changes"];
+                
+                for (var j = 0; j < q.length; j++) {
+                    var stateChange = q[j];
+
+                    //iterate through all events and then trigger the actions and transitions
+                    var types = stateChange["type"] instanceof Array ? stateChange["type"] : [stateChange["type"]];
+                    for (var u = 0; u < types.length; u++) {
+                        if (types[u] != this.lastEvent.type)
+                            continue;
+
+                        //guard is true?
+                        var guardValue = true;
+                        if (stateChange.hasOwnProperty("guard")) {
+                            var f = stateChange["guard"];
+                            var transtioner = this.context;
+                            guardValue = f();
                         }
-                    }
-                    
-                    //do actions
-                    if(stateChange.hasOwnProperty("actions") && stateChange["actions"] != null) {
-                        for(var k2:number=0; k2< stateChange["actions"].length; k2++) {
-                            (new stateChange["actions"][k2](this._context)).run();
-                            //									trace("    actions: "+stateChange["actions"][k2].toString());
-                        }
-                    }
-                    
-                    //                    //do binding to input
-                    //                    if(stateChange.hasOwnProperty("bind") && stateChange["bind"] != null) {
-                    //                        var o:Object = stateChange["bind"];
-                    //                        this.changeWatchers.push(BindingUtils.bindProperty(this._view, o.outlet, o.device, o.property)); 
-                    //                    }
-                    //                    if(stateChange.hasOwnProperty("unbind") && stateChange["unbind"] != null) {
-                    //                        for (var i2=0;i<this.changeWatchers.length;i++) 
-                    //                            this.changeWatchers[i2].unwatch();
-                    //                    }
-                    //                    
-                    //do focus change
-                    if(stateChange.hasOwnProperty("focusActivate") && stateChange["focusActivate"] != null) {
-                        var c:String = stateChange["focusActivate"][0];
-                        FocusModel.instance.activate(this._view[c].controllers[0]);
-                    }
-                    if(stateChange.hasOwnProperty("focusDeactivate") && stateChange["focusDeactivate"] != null) {
-                        var c2:String = stateChange["focusDeactivate"];
-                        FocusModel.instance.deactivate(this._view[c2].controllers[0]);
-                    }							
-                    //							
-                    //do focusChange
-                    //														if(stateChange.hasOwnProperty("focusChange") && stateChange["focusChange"] != null) {
-                    //															var c:Class = stateChange["focusChange"];
-                    //							//								FocusModel.instance.transferFocus(
-                    //							//									FocusModel.instance.focusComponentStack[FocusModel.instance.focusComponentStack.length-1],
-                    //							//									c);
-                    //							//								trace("focusChange: "+Object(FocusModel.instance.focusComponentStack[FocusModel.instance.focusComponentStack.length-1]).toString()+"to: "
-                    //							//									+c.toString());
-                    //															for(var k3:number=0; k3< FocusModel.instance.availableViews.length; k3++) {
-                    //																if(FocusModel.instance.availableViews[k3] is c) {
-                    //																	FocusModel.instance.transferFocus(
-                    //																		FocusModel.instance.focusComponentStack[FocusModel.instance.focusComponentStack.length-1],
-                    //																		c.controller);
-                    //																		trace("focusChange: "+Object(FocusModel.instance.focusComponentStack[FocusModel.instance.focusComponentStack.length-1]).toString()+"to: "
-                    //																		+Object(c.controller).toString());
-                    //																}
-                    //																
-                    //															}
-                    //														}
-                    
-                    if(!stateChange.hasOwnProperty("newState")) return;
-                    //store new state
-                    //							trace("    new state: "+stateChange["newState"]);
-                    
-                    var oldState:String = this._context["state"];							
-                    if(oldState == stateChange["newState"]) 
-                    {
-                        // trace("    new state: "+stateChange["newState"]);
-                        this._context["state"]  =  stateChange["newState"];
-                        return;
-                    }
-                    
-                    // search for new state in the model and execute onLeave methods
-                    if(s.hasOwnProperty("onLeave") && s["onLeave"] != null) {
-                        for(k=0; k< s["onLeave"].length; k++) {
-                            (new s["onLeave"][k](this._context)).run();											
-                        }									
-                    }
-                    
-                    // trace("    new state: "+stateChange["newState"]);
-                    this._context["state"]  =  stateChange["newState"];
-                    
-                    // search for new state in the model and execute onEnter methods
-                    for(j=0; j< this._model.length; j++) {
-                        s = this._model[j];
-                        if(this._context["state"] == s["state"]) {
-                            if(s.hasOwnProperty("onEnter") && s["onEnter"] != null) {
-                                for(k=0; k< s["onEnter"].length; k++) {
-                                    (new s["onEnter"][k](this._context)).run();											
-                                }									
+                        if (!guardValue)
+                            continue;
+
+                        console.log(this.lastEvent+ " "+(stateChange["transition"][k]));
+                        if (stateChange.hasOwnProperty("transition") && stateChange["transition"] != null) {
+                            for (var k = 0; k < stateChange["transition"].length; k++) {
+                                 console.log(stateChange["transition"][k]);
+                                (stateChange["transition"][k]).call(this.context, this.lastEvent);
                             }
                         }
+
+                        if (stateChange.hasOwnProperty("actions") && stateChange["actions"] != null) {
+                            for (var k2 = 0; k2 < stateChange["actions"].length; k2++) {
+                                (stateChange["actions"][k2]).call(this.context);
+                            }
+                        }
+
+                        if (stateChange.hasOwnProperty("focusActivate") && stateChange["focusActivate"] != null) {
+                            var c = stateChange["focusActivate"][0];
+                            FocusModel.instance.activate(this.view[c].controllers[0]);
+                        }
+                        if (stateChange.hasOwnProperty("focusDeactivate") && stateChange["focusDeactivate"] != null) {
+                            var c2 = stateChange["focusDeactivate"];
+                            FocusModel.instance.deactivate(this.view[c2].controllers[0]);
+                        }
+
+                        if (!stateChange.hasOwnProperty("newState"))
+                            return;
+                        var oldState = this.context["state"];
+                        if (oldState == stateChange["newState"]) {
+                            this.context["state"] = stateChange["newState"];
+                            return;
+                        }
+
+                        if (s.hasOwnProperty("onLeave") && s["onLeave"] != null) {
+                            for (k = 0; k < s["onLeave"].length; k++) {
+                                (s["onLeave"][k]).call(this.context);
+                            }
+                        }
+
+                        this.context["state"] = stateChange["newState"];
+
+                        for (j = 0; j < this.flow.length; j++) {
+                            s = this.flow[j];
+                            if (this.context["state"] == s["state"]) {
+                                if (s.hasOwnProperty("onEnter") && s["onEnter"] != null) {
+                                    for (k = 0; k < s["onEnter"].length; k++) {
+                                        (s["onEnter"][k]).call(this.context);
+                                    }
+                                }
+                            }
+                        }
+                        return;
                     }
-                    return;	
                 }
             }
         }
     }
-} 
 
-set_view(v):void { 
-    if(this._view==v) return; 
-    this._view = v; 
-    FocusModel.instance.availableViews.push(v); 
-    this.setup(); 
-    this.processUserInput("viewAttached");
-}
+    get view() { return this._view ; }
+    set view(v) { 
+            if (this._view == v) return;
+            this._view = v;
+            this.setup();
+            FocusModel.instance.availableViews.push(v);
+            this.processUserInput(new Eventl("viewAttached"));
+    }
 }
