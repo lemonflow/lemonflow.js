1c04fa3344a03be246cffc32d7336197741d2639
Philipp Fischer <philipp.fischer@daimler.com>
2013-12-23 21:38:34 +0100

    moved old reference files to fodler "backup"

--- manual/UIContext.js
+++
@@ -1,9 +0,0 @@
-UIContext.prototype = Object.create(BaseContext.prototype);
-
-function UIContext() {
-    BaseContext.apply(this, arguments);
-
-    this.state                 = "main";
-    this.view                  = null;
-    this.controller            = null;
-}
